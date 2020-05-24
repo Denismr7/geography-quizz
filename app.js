8d5f3099cec732dc559468554df5e9e98c93ef52
Denis Mory <denismr7@gmail.com>
2020-05-24 12:02:47 +0200

    minor improvements, refactorized and added more countries

--- app.js
+++ app.js
@@ -50,6 +50,56 @@ const db = [
         capital: "Reikiavik",
         continent: "Europa"
     },
+    {
+        country: "India",
+        capital: "Nueva Delhi",
+        continent: "Asia"
+    },
+    {
+        country: "Irán",
+        capital: "Teherán",
+        continent: "Asia"
+    },
+    {
+        country: "Japón",
+        capital: "Tokio",
+        continent: "Asia"
+    },
+    {
+        country: "Líbano",
+        capital: "Beirut",
+        continent: "Asia"
+    },
+    {
+        country: "China",
+        capital: "Pekín",
+        continent: "Asia"
+    },
+    {
+        country: "Nepal",
+        capital: "Katmandu",
+        continent: "Asia"
+    },
+    {
+        country: "Qatar",
+        capital: "Doha",
+        continent: "Asia"
+    },
+    {
+        country: "Corea del Sur",
+        capital: "Seúl",
+        continent: "Asia"
+    },
+    {
+        country: "Tailandia",
+        capital: "Bangkok",
+        continent: "Asia"
+    },
+    {
+        country: "Taiwán",
+        capital: "Taipei",
+        continent: "Asia"
+    },
 ]
 
 // Selectors
@@ -60,19 +110,20 @@ const btn = document.querySelector(".quizzContainer__btn");
 
 // Variables
 let currentQuestionNum = 1;
-let randomCountryName = db[Math.floor(Math.random() * db.length)].country;
+let randomCountryName;
 let score = 0;
+let numberOfQuestions = 5;
 
 // Functions
-function generateRandomCountryName() {
+function generateRandomCountryName(db) {
     return db[Math.floor(Math.random() * db.length)].country;
 }
 
 function startQuizz() {
         score = 0;
         currentQuestionNum = 1;
-        randomCountryName = generateRandomCountryName();
-        title.textContent = `Pregunta ${currentQuestionNum} de ${db.length}`;
+        randomCountryName = generateRandomCountryName(db);
+        title.textContent = `Pregunta ${currentQuestionNum} de ${numberOfQuestions}`;
         subtitle.textContent = `¿Cuál es la capital de ${randomCountryName}?`
         input.style.opacity = 1;
         input.style.pointerEvents = "all";
@@ -83,8 +134,8 @@ function startQuizz() {
         // Add new Event Listener
         btn.addEventListener("click", nextQuestion)
 }
-function checkResponse() {
-    const response = input.value.toLowerCase();
+function checkResponse(userResponse, db) {
+    const response = userResponse.toLowerCase();
     let result = false;
     db.forEach(element => {
        if (element.capital.toLowerCase() === response) {
@@ -98,21 +149,27 @@ function checkResponse() {
     return result;
 }
 function nextQuestion() {
-    if (currentQuestionNum < db.length) {
-        if (checkResponse()) {
+    if (currentQuestionNum < numberOfQuestions) {
+        if (checkResponse(input.value, db)) {
             score += 1;
         }
         input.value = "";
         currentQuestionNum += 1;
-        randomCountryName = generateRandomCountryName();
-        title.textContent = `Pregunta ${currentQuestionNum} de ${db.length}`;
+        randomCountryName = generateRandomCountryName(db);
+        title.textContent = `Pregunta ${currentQuestionNum} de ${numberOfQuestions}`;
         subtitle.textContent = `¿Cuál es la capital de ${randomCountryName}?`;
     } else {
-        checkResponse() ? score += 1 : score;
-        title.textContent = "Test finalizado"
-        subtitle.textContent = `Tu puntuación es de ${score} sobre 10`
+        checkResponse(input.value, db) ? score += 1 : score;
+        if (score < Math.floor(numberOfQuestions / 2)) {
+            title.textContent = "Ohh! Seguro que la próxima vez tienes más suerte!"
+        } else if (score === numberOfQuestions) {
+            title.textContent = "¡Pleno! Has acertado todas"
+        } else if (score >= Math.floor(numberOfQuestions/2)) {
+            title.textContent = "¡No se te da nada mal!"
+        }
+        subtitle.textContent = `Tu puntuación es de ${score} sobre ${numberOfQuestions}`
         input.value = "";
-        btn.innerText = "Jugar de nuevo"
+        btn.innerText = "Repetir"
 
         btn.removeEventListener("click", nextQuestion)
         btn.addEventListener("click", startQuizz)
